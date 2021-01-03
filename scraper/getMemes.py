543b5781f0bf2fae0f2df5aee3ddc84aca00ec3c
almondwilde <human.ipod7@gmail.com>
2021-01-02 19:41:00 -0800

    start getMemes script

--- scraper/getMemes.py
+++ scraper/getMemes.py
@@ -1,5 +1,13 @@
 import instagram_scraper as insta
 
-i = insta.InstagramScraper
-print(i)
+scraper = insta.InstagramScraper(login_user='theemptytrashbag',
+ login_pass='8utt3r_5c0tch', filename='ig-users.txt', interactive = True,
+ quiet=False, maximum = 0, media_metadata=True, log_destination='logs/',
+  latest=True, latest_stamps=True, tag=)
 
+scraper.authenticate_with_login()
+
+scraper.logout()
+scraper.scrape()
+#scraper.scrape_hashtag()
+print(scraper.username)
