de0b94c9072854303fdc54a3b02ea3e52166e14b
almondwilde <human.ipod7@gmail.com>
2021-01-02 20:49:54 -0800

    update scraper

--- scraper/getMemes.py
+++ scraper/getMemes.py
@@ -3,11 +3,11 @@ import instagram_scraper as insta
 scraper = insta.InstagramScraper(login_user='theemptytrashbag',
  login_pass='8utt3r_5c0tch', filename='ig-users.txt', interactive = True,
  quiet=False, maximum = 0, media_metadata=True, log_destination='logs/',
-  latest=True, latest_stamps=True, tag=)
+  latest=True, latest_stamps=True)
 
 scraper.authenticate_with_login()
+scraper.scrape()
 
 scraper.logout()
-scraper.scrape()
 #scraper.scrape_hashtag()
 print(scraper.username)
