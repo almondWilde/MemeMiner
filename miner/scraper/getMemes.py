ec88c7052774ff8503ebe0cf1b1b09462161be5b
almondwilde <human.ipod7@gmail.com>
2021-02-17 23:22:20 -0800

    completed tag scraping

--- miner/scraper/getMemes.py
+++ miner/scraper/getMemes.py
@@ -1,25 +1,30 @@
 #run on ubuntu
 import os
 import json
+import random
+import time
 
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 import instagram_scraper as insta
 
 #quiet but keeps Logs
 #100 posts per Scraping
-tag_scraper = insta.InstagramScraper(login_user='memescraperproject', login_pass='*********', interactive = False, quiet=False, maximum = 1, media_metadata=True, log_destination='logs/', latest=True,tag=True, filename='tags.txt')
+tag_scraper = insta.InstagramScraper(login_user='memescraperproject', login_pass='*********', media_types=['image'], interactive = False, quiet=False, maximum = 50, media_metadata=True, log_destination='logs/', latest=True,tag=True, filename='tags.txt')
 
-users_scraper = insta.InstagramScraper(login_user='memescraperproject', login_pass='*******', interactive = False, quiet=False, maximum = 1, media_metadata=True, log_destination='logs/', latest=True,tag=False, filename='ig-users.txt')
+users_scraper = insta.InstagramScraper(login_user='memescraperproject', login_pass='Bobmade1box', media_types=['image'], interactive = False, quiet=False, maximum = 50, media_metadata=True, log_destination='logs/', latest=True,tag=False, filename='ig-users.txt')
 
 
 #this loop will scrape by tags first then users
-for medium in ('u', 't'):
+#for medium in ('u', 't'):
+for medium in ('t'):
     #assign the input file
     if medium == 'u':
         if users_scraper.login_user and users_scraper.login_pass:
             print("pre-auth")
             users_scraper.authenticate_with_login()
             print("post auth")
+        if not users_scraper.authenticated:
+            continue
 
         print("User input file:", users_scraper.filename)
         with open(users_scraper.filename, "r") as userf:
@@ -32,13 +37,26 @@ for medium in ('u', 't'):
     if medium == 't':
         if tag_scraper.login_user and tag_scraper.login_pass:
             print("pre-auth")
-            tag_scraper.authenticate_with_login()
+            #tag_scraper.authenticate_with_login()
             print("post auth")
 
         print("Tag input file:", tag_scraper.filename)
+
+        #last off
         with open(tag_scraper.filename, "r") as tagsf:
-            tag_scraper.usernames = tagsf.readlines()
-        tag_scraper.usernames = [x.strip() for x in tag_scraper.usernames]
+            count = 0
+            random.seed(time.time(),1)
+            step = int(random.random()*30)+1
+            max = 10
+
+            while tagsf.readable() and (len(tag_scraper.usernames) <= max):
+                tag = tagsf.readline()
+                if count % step == 0:
+                    tag_scraper.usernames.append( tag.strip())
+                    print(tag, ': step count nusernames length:\n', step, count, len(tag_scraper.usernames))
+                count = count + 1
+
+        #tag_scraper.usernames = [x.strip() for x in tag_scraper.usernames]
 
         try:
             tag_scraper.scrape_hashtag()
