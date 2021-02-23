b5ecee75d6c0f87d87745cdee90073d41e5c8e04
almondWilde <human.ipod7@gmail.com>
2021-02-22 16:52:52 -0800

    added 101 image set for testing + added web examples

---
+++ webserver/Infinite Scroll/App.vue
@@ -0,0 +1,25 @@
+var app = new Vue({
+          el: '#app',
+          data: {
+            posts: [],
+            limit: 10,
+            busy: false
+          },
+          created(){
+              this.loadMore();
+          },
+          methods: {
+            loadMore(){
+              console.log("Adding 10 more data results");
+                this.busy = true;
+                axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+                  const append = response.data.slice(
+                    this.posts.length,
+                    this.posts.length+ this.limit
+                  );
+                  this.posts = this.posts.concat(append);
+                  this.busy = false;
+                });
+            }
+          }
+        });
